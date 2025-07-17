4d8ec2cbbd9c72a9009771b6ec5228fa62f1bfe2
Hanif Royyan <hanifroyyan@Hanifs-MacBook-Pro.local>
2025-07-17 11:26:01 +0700

    push prj

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gpn-frontend/',
+  // base: '/gpn-frontend/',
   plugins: [
     react(),
     tailwindcss()
