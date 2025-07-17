964b5c0a10d3678f40d636924a7c1651f422b7ce
Hanif Royyan <hanifroyyan@Hanifs-MBP.bbrouter>
2025-07-17 19:53:25 +0700

    gallery fix

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,9 +1,15 @@
-
+import { useState } from 'react';
 import Footer from '../assets/components/Footer';
 import Navbar from '../assets/components/Navbar';
 import Cta from '../assets/components/Cta';
 
 const Gallery = () => {
+  const [visibleCount, setVisibleCount] = useState(8); // jumlah awal gambar
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 4); // tambah 4 gambar setiap klik
+  };
+
   return (
     <>
       <Navbar />
@@ -15,7 +21,35 @@ const Gallery = () => {
           <p className="text-lg font-medium mb-0">Dokumentasi foto dari berbagai kegiatan Gerakan Pemuda Natuna yang penuh aksi dan inspirasi</p>
         </div>
       </section>
+      <section id='img-gallery' className='bg-white mb-16'>
+        <div className='max-w-7xl mx-3 md:mx-auto'>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+            {[...Array(50)].slice(0, visibleCount).map((_, i) => (
+              <a href="#" key={i} className="flex-shrink-0 w-full">
+                <div className="bg-blue-100 hover:bg-blue-200 transform hover:scale-105 transition duration-200 ease-out rounded-lg overflow-hidden">
+                  <img
+                    src={`https://picsum.photos/300/200?random=${i}`}
+                    alt={`Image ${i}`}
+                    className="w-full h-[250px] object-cover"
+                  />
+                </div>
+              </a>
+            ))}
+          </div>
 
+          {/* Tombol Load More */}
+          {visibleCount < 50 && (
+            <div className="text-center mt-8">
+              <button
+                onClick={handleLoadMore}
+                className="text-sm px-7 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition"
+              >
+                Load More
+              </button>
+            </div>
+          )}
+        </div>
+      </section>
       <Cta />
       <Footer />
     </>
